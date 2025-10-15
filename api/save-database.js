c922167f7608b186b3c3e230dd79f990bd0fe948
SKLD-1971 <rashodalkitbe2020@gmail.com>
2025-10-15 18:12:15 +0400

    Update save-database.js

--- api/save-database.js
+++ api/save-database.js
@@ -2,15 +2,13 @@ import { kv } from '@vercel/kv';
 
 export default async function handler(request, response) {
   if (request.method !== 'POST') {
-    return response.status(405).json({ message: 'Only POST requests are allowed.' });
+    return response.status(405).end();
   }
 
   try {
-    const newData = request.body; // Vercel automatically parses the JSON body
-    await kv.set('database', newData);
-    return response.status(200).json({ message: 'Database updated successfully in Vercel KV.' });
+    await kv.set('database', request.body);
+    return response.status(200).json({ message: 'Database updated.' });
   } catch (error) {
-    console.error(error);
-    return response.status(500).json({ message: 'Error saving data to Vercel KV.' });
+    return response.status(500).json({ message: 'Error saving data.' });
   }
 }
